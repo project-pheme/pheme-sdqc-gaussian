37b5039a12e52c4e4dab75b88c874123c619cbf1
Michal dell latitude <michal.mateusz.lukasik@gmail.com>
2015-10-13 20:37:59 +0100

    kafkaa code

--- kafka_consumer_misinformation.py
+++ kafka_consumer_misinformation.py
@@ -8,7 +8,7 @@ from add_misinformation_to_json import process_jsons
 import pickle
 import os
 FILE_TMP="file_tmp.txt"
-#FILE_TMP2="file_tmp2.txt"
+FILE_TMP2="file_tmp2.txt"
 
 #topicin="test1"#sys.argv[1]
 #topicout="test2"#sys.argv[2]
@@ -28,18 +28,12 @@ with client.topics[topicout].get_producer() as producer:
     for msg in consumer:
         if msg is not None:
             print msg.offset, msg.value
-            #try:
-            #with open(FILE_TMP, 'w') as ftmp:
-            #    ftmp.write(msg.value)
-            command="echo "+msg.value+" | python text.py -t txt -r text -p 19,0,15,5,3,6,4,20,8 -w BROWN_STR -b data/resources/50mpaths2 -j lines > "+FILE_TMP
-            #print "command", command
+            with open(FILE_TMP2, 'w') as ftmp:
+                ftmp.write(msg.value)
+            command="cat "+FILE_TMP2+" | python text.py -t txt -r text -p 19,0,15,5,3,6,4,20,8 -w BROWN_STR -b data/resources/50mpaths2 -j lines > "+FILE_TMP
             os.system(command)
             with open(FILE_TMP, 'r') as ftmp:
                 msg.value=ftmp.readline()
-            #print "msg.value:", msg.value
-            #print "[json.loads(msg.value)]:", [json.loads(msg.value)]
             for res in process_jsons(json.loads(msg.value), header, m):
                 print "Added misinformation to a json, resulting in the following json:", res
                 producer.produce(json.dumps(res))
-            #except:
-            #    print "Error in kafka processing! Wrong input format?"
