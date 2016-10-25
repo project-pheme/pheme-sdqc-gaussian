fd6f68f9d3169e6111a89f5156ac8ef473061bce
Leon Derczynski <leon@gatestorm1.dcs.shef.ac.uk>
2016-10-25 13:10:05 +0100

    remove parallel client clashes, add better debugging output, tune to this py deployment

--- kafka_consumer_misinformation.py
+++ kafka_consumer_misinformation.py
@@ -8,8 +8,9 @@ from pykafka import KafkaClient
 from add_misinformation_to_json import process_jsons
 import pickle
 import os
-FILE_TMP="file_tmp.txt"
-FILE_TMP2="file_tmp2.txt"
+import time
+FILE_TMP=str(os.getpid())+"_a.tmp"
+FILE_TMP2=str(os.getpid())+"_b.tmp"
 
 #topicin="test1"#sys.argv[1]
 #topicout="test2"#sys.argv[2]
@@ -22,8 +23,10 @@ modelpath=sys.argv[4]
 
 header=open(trainingdatapath).readline().split()
 m=pickle.load(open(modelpath, "r"))
-client = KafkaClient(hosts="127.0.0.1:9092")
+client = KafkaClient(hosts="gatezkt1.storm:9092")
 consumer = client.topics[topicin].get_simple_consumer()
+
+print time.strftime('%c'), 'starting'
 
 with client.topics[topicout].get_producer() as producer:
     for msg in consumer:
@@ -34,9 +37,9 @@ with client.topics[topicout].get_producer() as producer:
                 print('bad json')
                 print(msg.value)
                 continue
-    	    if msgval['lang'] != 'en':
-                continue
-            print('Processing', msgval['id'])
+#            if msgval['lang'] != 'en':
+#                continue
+#            print('Processing', msgval['id'])
             with open(FILE_TMP2, 'w') as ftmp:
                 try:
                     ftmp.write(json.dumps(msgval))
@@ -44,11 +47,19 @@ with client.topics[topicout].get_producer() as producer:
                     print('Failed to write to', FILE_TMP2)
                     print(e)
                     continue
-            command="cat "+FILE_TMP2+" | python text.py -t txt -r text -p 19,0,15,5,3,6,4,20,8 -w BROWN_STR -b data/resources/50mpaths2 -j lines > "+FILE_TMP
+            command="cat "+FILE_TMP2+" | ~/sdqc2/bin/python2 text.py -t txt -r text -p 19,0,15,5,3,6,4,20,8 -w BROWN_STR -b data/resources/50mpaths2 -j lines > "+FILE_TMP
             os.system(command)
             with open(FILE_TMP, 'r') as ftmp:
                 augmented=ftmp.readline()
-            for res in process_jsons(json.loads(augmented), header, m):
+
+            try:
+                j = json.loads(augmented)
+            except:
+                print('Failed to load augmented JSON:')
+                print(augmented)
+                sys.exit()
+
+            for res in process_jsons(j, header, m):
                 # print "Added misinformation to a json, resulting in the following json:", res
-                print(msg.partition.id, msg.offset, res['pheme_sdqc'], json.loads(augmented)[0]['text'][:100])
+                print time.strftime('%c'), msg.partition.id, msg.offset, res['pheme_sdqc'], json.loads(augmented)[0]['text'][:100]
                 producer.produce(json.dumps(res))
